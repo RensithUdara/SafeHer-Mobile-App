f9518bc8c93a50615bb8e81e8712a8531a45caa9
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-29 19:38:23 +0530

    Fix Facebook sign-in credential token usage in AuthService

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -163,10 +163,8 @@ class AuthService {
       }
 
       // Create a credential from the access token
-      final OAuthCredential facebookAuthCredential =
-          FacebookAuthProvider.credential(loginResult.accessToken!.tokenString);
-
-      // Sign in to Firebase with the Facebook credential
+      final OAuthCredential facebookAuthCredential = 
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);      // Sign in to Firebase with the Facebook credential
       final UserCredential userCredential =
           await _auth.signInWithCredential(facebookAuthCredential);
 
