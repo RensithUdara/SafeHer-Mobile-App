2dd040a96add9bec873cd2fa434dc98bc142dfd5
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-29 19:23:45 +0530

    Register additional plugins for enhanced functionality on Windows

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,18 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  battery_plus
+  cloud_firestore
+  connectivity_plus
+  file_selector_windows
+  firebase_auth
+  firebase_core
+  firebase_storage
+  flutter_secure_storage_windows
+  geolocator_windows
+  local_auth_windows
+  permission_handler_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
