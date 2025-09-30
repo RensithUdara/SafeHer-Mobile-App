36d41160a8e1650cf8baeee9cddf8c717e6c9ed7
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-30 13:43:12 +0530

    Move 'dart:ui' import to the top for consistency

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'dart:ui';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
