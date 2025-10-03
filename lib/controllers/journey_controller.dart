f44d719bd7a3846dbf086179eceaa54d34b93448
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-10-03 12:27:37 +0530

    Fix: Comment out location tracking implementation for future updates

--- lib/controllers/journey_controller.dart
+++ lib/controllers/journey_controller.dart
@@ -203,20 +203,20 @@ class JourneyController extends ChangeNotifier {
     try {
       // TODO: Fix location tracking implementation
       // _positionStream = _locationService.getPositionStream()?.listen(
-      //   (Position position) {
-      //     _routeCoordinates.add(position);
-      //     _updateJourneyLocation(position);
+      //     (Position position) {
+      //       _routeCoordinates.add(position);
+      //       _updateJourneyLocation(position);
       //
-      //     // Check for route deviation
-      //     if (_enableRouteDeviation) {
-      //       _checkRouteDeviation(position);
-      //     }
+      //       // Check for route deviation
+      //       if (_enableRouteDeviation) {
+      //         _checkRouteDeviation(position);
+      //       }
       //
-      //     notifyListeners();
-      //   },
-      //   onError: (error) {
-      //     debugPrint('Location tracking error: $error');
-      //   },
+      //       notifyListeners();
+      //     },
+      //     onError: (error) {
+      //       debugPrint('Location tracking error: $error');
+      //     },
       // );
     } catch (e) {
       debugPrint('Error starting location tracking: $e');
